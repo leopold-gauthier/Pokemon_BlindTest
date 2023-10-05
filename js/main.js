732088d061246dd05a8a7226175ec4fa26a2e0d1
unknown <Administrateur@MLP.m2iformation.local>
2023-10-05 10:16:11 +0200

    ajout conditions vue4

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const gameStart = document.querySelector(".game-start");
 const mainGame = document.querySelector(".main-game");
 const pkmnImg = document.querySelector("#pkmn-img");
 const showResponses = document.querySelector(".show-respones");
+const showBtn = document.querySelector("#showBtn");
 const results = document.querySelector(".results");
 const gameTitle = document.querySelector("#gameTitle");
 // boutons users
@@ -21,14 +22,19 @@ const next = document.querySelector("#next");
 
 // variables globales
 let choiceMod = "";
-
+let isClicked = false;
 //FUNCTION GAME CHOICE
+
+function ShowHidden(target, nextTarget) {
+    target.classList.toggle("d-none");
+    nextTarget.classList.toggle("d-none");
+}
 
 function gameSelector() {
 
     blurBox.addEventListener("click", () => {
         choiceMod = "blur";
-        pkmnImg.classList.add("blur");
+        pkmnImg.style.filter = "blur(40)";
         gameTitle.textContent = "Jeu : Pokémon Flou"; // Définit le titre du jeu
         console.log("blur");
         ShowHidden(gameSelect, playerSelector);
@@ -37,7 +43,6 @@ function gameSelector() {
 
     shadowBox.addEventListener("click", () => {
         choiceMod = "shadow";
-        pkmnImg.style.filter = "brightness(0)";
         pkmnImg.classList.add("shadow");
         gameTitle.textContent = "Jeu : Who's That Pokémon?"; // Définit le titre du jeu
         console.log("shadow");
@@ -46,9 +51,22 @@ function gameSelector() {
     
 }
 
-function ShowHidden(target, nextTarget) {
-    target.classList.toggle("d-none");
-    nextTarget.classList.toggle("d-none");
+
+
+// vue 1 -------
+gameSelector();
+
+// vue 2 -------
+
+// vue 3 -------
+
+// vue 4 -------
+showBtn.addEventListener("click", ()=> isClicked = true);
+if (choiceMod == "blur") {
+    blur(pkmnImg);
+} else {
+    pkmnImg.classList.remove("shadow");
 }
 
-gameSelector();
+
+// vue 5 -------
